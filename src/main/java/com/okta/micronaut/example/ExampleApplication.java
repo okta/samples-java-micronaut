f186dd9df22b128f04ebdcfa3609e45f64cf5957
Arvind Krishnakumar <61501885+arvindkrishnakumar-okta@users.noreply.github.com>
2020-09-21 16:44:03 -0700

    Developmental commit

---
+++ src/main/java/com/okta/micronaut/example/ExampleApplication.java
@@ -0,0 +1,10 @@
+package com.okta.micronaut.example;
+
+import io.micronaut.runtime.Micronaut;
+
+public class ExampleApplication {
+
+    public static void main(String[] args) {
+        Micronaut.run(ExampleApplication.class);
+    }
+}
---
+++ src/main/java/com/okta/micronaut/example/HomeController.java
@@ -0,0 +1,22 @@
+package com.okta.micronaut.example;
+
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.rules.SecurityRule;
+import io.micronaut.views.View;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class HomeController {
+
+    @Secured(SecurityRule.IS_ANONYMOUS)
+    @View("home")
+    @Get
+    public Map<String, Object> index() {
+        return Collections.emptyMap();
+    }
+}
