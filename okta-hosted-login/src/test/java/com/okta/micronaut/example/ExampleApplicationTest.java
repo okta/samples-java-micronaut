9fa63a8f1c246d8e17358769f2071ce3786b00a7
Arvind Krishnakumar <arvind.krishnakumar@okta.com>
2023-06-01 14:12:18 -0500

    test updated

--- okta-hosted-login/src/test/java/com/okta/micronaut/example/ExampleApplicationTest.java
+++ okta-hosted-login/src/test/java/com/okta/micronaut/example/ExampleApplicationTest.java
@@ -1,21 +1,25 @@
 package com.okta.micronaut.example;
 
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
 import io.micronaut.runtime.EmbeddedApplication;
-import io.micronaut.test.annotation.MicronautTest;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
 
-import javax.inject.Inject;
+import static org.junit.jupiter.api.Assertions.*;
 
 @MicronautTest
 public class ExampleApplicationTest {
 
     @Inject
-    EmbeddedApplication application;
+    EmbeddedApplication<?> application;
 
     @Test
-    void testItWorks() {
-        Assertions.assertTrue(application.isRunning());
+    void testHello(@Client("/") HttpClient client) {
+        String message = client
+                .toBlocking()
+                .retrieve("/");
+        assertTrue(message.contains("Okta Hosted Login + Micronaut Example"));
     }
-
 }
